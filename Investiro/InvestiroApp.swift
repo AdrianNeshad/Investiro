51a650fad13d10dce13173dc1e8886fd73e979d4
Adrian <adrian.neshad1@gmail.com>
2025-11-02 12:44:51 +0100

    Initial Commit

---
+++ Investiro/InvestiroApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestiroApp.swift
+//  Investiro
+//
+//  Created by Adrian Neshad on 2025-11-02.
+//
+
+import SwiftUI
+
+@main
+struct InvestiroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
